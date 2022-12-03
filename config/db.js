2cb7633f5c319d7fffdaa59aae784056b31c50e1
vlauncher <v2dent@gmail.com>
2022-12-03 07:44:53 +0100

    my first

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-// const database = new Sequelize({
-//     dialect : 'sqlite',
-//     storage : 'db.sqlite3'
-// });
+const database = new Sequelize({
+    dialect : 'sqlite',
+    storage : 'db.sqlite3'
+});
 
-const database = new Sequelize('postgres://yaydtkgi:l7SSFDNU7zxkoDGyN5l03dKseWB_d-Az@peanut.db.elephantsql.com/yaydtkgi')
+// const database = new Sequelize('postgres://yaydtkgi:l7SSFDNU7zxkoDGyN5l03dKseWB_d-Az@peanut.db.elephantsql.com/yaydtkgi')
 
 database.sync();
 
